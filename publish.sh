fd70c03807fa01ab1781a34a68195408218f6441
Tobias Gerstenberg <tobiace@gmail.com>
2017-11-21 13:02:40 -0500

    update

--- publish.sh
+++ publish.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-
+# $1='commit message'
 cd public
 git add --all
-git commit -m .
+git commit -m $1
 git push 
 
 cd .. 
 
 git add --all 
-git commit -m . 
+git commit -m $1 
 git push 
