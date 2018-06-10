6cc3dff474d9365bf7224325513f71777a347b38
Ari Beller <aribeller@gmail.com>
2018-06-10 16:58:47 -0400

    change forthcoming info in bibtex from journal entry to year entry

--- parser_tests/parse_tester.py
+++ parser_tests/parse_tester.py
@@ -15,6 +15,11 @@ with open(inputfile, encoding='utf8') as bibtex_file:
 
 print()
 
+# This prints the markdown formatted citations for the publications in the test
+# file. It's an eyeball test. I was going to make the exam more rigid and comprehensive
+# but ultimately determined time could better be spent solving other problems since
+# the citation generator seemed to be in mostly good shape. Could be something to come
+# back to time permitting -- Ari Beller
 for entry in bib_database.entries:
 	print(c.construct_citation(entry))
 	print()
