7bc740e8deda55aeee46b362510a0f466616cfd7
Tobias Gerstenberg <tobiace@gmail.com>
2018-08-24 10:14:23 -0700

    reverted to old upload script

--- upload.sh
+++ upload.sh
@@ -15,19 +15,6 @@ git push
 
 cd .. 
 
-cd themes/academic
-
-git add --all 
-if [ "$1" != "" ] 
-    then
-        git commit -m "$1"
-    else
-        git commit -m "update"
-    fi
-git push 
-
-cd ../..
-
 git add --all 
 if [ "$1" != "" ] 
     then
